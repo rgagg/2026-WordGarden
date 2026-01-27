763040e54d260d72fd61a722d1a4ed63c299f976
Richard Gagg <r.gagg@outlook.com>
2026-01-27 11:00:57 +1100

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Richard Gagg on 27/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
