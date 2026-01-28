52dccb891b2cf1c29f9294ad587f1ec0331dbbe3
Richard Gagg <r.gagg@outlook.com>
2026-01-28 16:16:20 +1100

    Ch: 2.2. Configured input keyboard. Configured input character.

--- WordGarden/ContentView.swift
+++ WordGarden/ContentView.swift
@@ -15,7 +15,8 @@ struct ContentView: View {
   @State private var currentWord: Int = 0 // Index of wordsToGuess array
   @State private var guessedLetter: String = ""
   @State private var imageName: String = "flower8"
-  @State private var playAgainIsHedden: Bool = true
+  @State private var playAgainIsHidden: Bool = true
+  @FocusState private var textFieldIsFocussed: Bool
   
   @State private var  wordsToGuess: [String] = [
     "SWIFT",
@@ -54,19 +55,32 @@ struct ContentView: View {
         .font(.title)
         .multilineTextAlignment(.center)
       
-      if playAgainIsHedden {
+      if playAgainIsHidden {
         HStack {
           TextField("", text: $guessedLetter)
+            .multilineTextAlignment(.center)
             .textFieldStyle(.roundedBorder)
             .frame(width: 35)
             .overlay {
               RoundedRectangle(cornerRadius: 5)
                 .stroke(.gray, lineWidth: 2)
             }
-          
+            .keyboardType(.asciiCapable)
+            .submitLabel(.done)
+            .autocorrectionDisabled(true)
+//            .textInputAutocapitalization(.characters)
+            .onChange(of: guessedLetter) {
+              guessedLetter = guessedLetter.trimmingCharacters(in: .letters.inverted)
+              guard let lastChar = guessedLetter.last else {
+                return
+              }
+              guessedLetter = String(lastChar).uppercased()
+            }
+            .focused($textFieldIsFocussed)
+         
           Button {
             // Guess Action
-            playAgainIsHedden = false
+            textFieldIsFocussed = false
           } label: {
             Text("Guess A Letter")
               .font(.title3)
@@ -75,13 +89,12 @@ struct ContentView: View {
           }
           .buttonStyle(.bordered)
           .tint(.mint)
+          .disabled(guessedLetter.isEmpty)
         }
       } else {
         
         Button {
           // Play Again Action
-          playAgainIsHedden = true
-
         } label: {
           Text("Play Again?")
             .font(.title3)
